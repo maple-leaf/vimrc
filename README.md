66eda4573c8aa032a0e73444b3dc3001fb76412a
maple-leaf <tangjf.88@163.com>
2014-05-29 13:38:20 +0800

    create seperatedConfig branch, and put in config files

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ vimrc
 =====
 
 my vimrc
-#vim config
+
+## Big Change(Base on spf13-vim and change many things to fit my need)
+* Custom version of [spf13-vim](https://github.com/spf13/spf13-vim)
+* Seperate vimrc to multi files
+* make plugins installation automatically using shell
+
---
+++ spf13-vim-windows-install.cmd
@@ -0,0 +1,34 @@
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+@set BASE_DIR=%HOME%\vimrc
+IF NOT EXIST "%BASE_DIR%" (
+  call git clone --recursive -b master https://github.com/maple-leaf/vimrc.git "%BASE_DIR%"
+) ELSE (
+	@set ORIGINAL_DIR=%CD%
+    echo updating vimrc
+    chdir /d "%BASE_DIR%" 
+	call git pull
+    chdir /d "%ORIGINAL_DIR%"
+	call cd "%BASE_DIR%" 
+)
+
+call mklink "%HOME%\.vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\_vimrc" "%BASE_DIR%\.vimrc"
+call mklink "%HOME%\.vimrc.fork" "%BASE_DIR%\.vimrc.fork"
+call mklink "%HOME%\.vimrc.bundles" "%BASE_DIR%\.vimrc.bundles"
+call mklink "%HOME%\.vimrc.bundles.fork" "%BASE_DIR%\.vimrc.bundles.fork"
+call mklink "%HOME%\.vimrc.before" "%BASE_DIR%\.vimrc.before"
+call mklink "%HOME%\.vimrc.before.fork" "%BASE_DIR%\.vimrc.before.fork"
+call mklink "%HOME%\.vimrc.functions" "%BASE_DIR%\.vimrc.functions"
+call mklink /J "%HOME%\.vim" "%BASE_DIR%\.vim"
+
+IF NOT EXIST "%BASE_DIR%\.vim\bundle" (
+	call mkdir "%BASE_DIR%\.vim\bundle"
+)
+
+IF NOT EXIST "%HOME%/.vim/bundle/vundle" (
+	call git clone https://github.com/gmarik/vundle.git "%HOME%/.vim/bundle/vundle"
+)
+
+call gvim -u "%BASE_DIR%/.vimrc.bundles" +BundleInstall! +BundleClean +qall
